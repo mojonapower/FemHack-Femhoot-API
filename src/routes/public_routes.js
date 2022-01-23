b1fa637b125ebd0d942ee0e8d60fe2bcf831bd8c
rosinni <ros.rm169@gmail.com>
2022-01-23 17:38:46 +0000

    database connection

--- src/routes/public_routes.js
+++ src/routes/public_routes.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const { append } = require('express/lib/response');
 const router = express.Router();
-const {response} = require('../actions/users')
-//importando controladores
-// const {createUser, getData, login} = require('../controllers/users.controllers');
+//import actions
+const {createUser, deleteUser, updateUser} = require('../actions/users.actions')
 
 
-//    =>/users
-//app.METODO('Route',(req,res)=>{
-
-//})
-// router.get('/', getData);
-// router.post('/users', createUser);
-// router.post('/login',login)
-router.get('/',response );
+//users routes
+router.post('/users',createUser);
+router.put('/users/:id',updateUser);
+router.delete('/users/:id',deleteUser);
 
 
 module.exports = router;
